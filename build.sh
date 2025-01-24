676bb7b6e6875f76c393a46bf08bd315b05da785
DanielGrenehed <daniel@acczo.com>
2025-01-24 12:42:49 +0100

    Fixed build script to build from anywhere and build package of current folder if another package is not specified

--- build.sh
+++ build.sh
@@ -4,31 +4,21 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 if [ -z $1 ]; then
   echo "No package specified"
   PWD=`pwd`
-  case $PWD in
-    *"packages/components"* | *"packages/app"* | *"packages/theme"*)
-      echo "Running build from ${PWD}";;
-    *)
+  if test -f package.json; then
+    echo "Running build from ${PWD}"
+  else 
       echo "Please run from a package or specify what package to build."
-      exit;;
-  esac
-fi
+      exit
+  fi
+else 
+  EXEC_DIR=${SCRIPT_DIR%%/}/packages/"$1"
+  if [ -d $EXEC_DIR ]; then
+    cd $EXEC_DIR
+  fi
+  PACKAGE=packages/"$1"
+  echo "Building ${PACKAGE} from:"
+  pwd
 
-cd $SCRIPT_DIR
-
-if [[ $1 = "components" ]]; then
-  cd packages/components
-  echo "Building packages/components from:"
-  pwd
-fi
-if [[ $1 = "app" ]]; then
-  cd packages/app
-  echo "Building packages/app from:"
-  pwd
-fi
-if [[ $1 = "theme" ]]; then
-  cd packages/theme
-  echo "Building packages/theme from:"
-  pwd
 fi
 
 npm run build &&
