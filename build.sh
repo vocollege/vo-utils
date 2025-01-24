bcd63dea3d0f9af00b388d589bbb661535f9c08b
DanielGrenehed <daniel@acczo.com>
2025-01-24 11:03:19 +0100

    Added package argument to build script

--- build.sh
+++ build.sh
@@ -1,3 +1,31 @@
 #!/bin/bash
+
+if [ -z $1 ]; then
+  echo "No package specified"
+  PWD=`pwd`
+  if [[ $PWD =~ "packages/components" ]] || [[ $PWD =~ "packages/app" ]] || [[ $PWD =~ "packages/theme" ]]; then
+    echo "Running build from ${PWD}"
+  else 
+    echo "Please run from a package or specify what package to build."
+    exit
+  fi
+fi
+
+if [[ $1 = "components" ]]; then
+  cd packages/components
+  echo "Building packages/components from:"
+  pwd
+fi
+if [[ $1 = "app" ]]; then
+  cd packages/app
+  echo "Building packages/app from:"
+  pwd
+fi
+if [[ $1 = "theme" ]]; then
+  cd packages/theme
+  echo "Building packages/theme from:"
+  pwd
+fi
+
 npm run build &&
 yalc push --replace --sig
